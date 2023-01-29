f9b96b9ed142c6f5ad31f200c6cf1003e1012739
Aliaksei <sladkiy.aleksey@gmail.com>
2023-01-29 19:10:10 +0100

    feat: add logic in routes index.js

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,17 +1,56 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { graphql, GraphQLSchema, GraphQLObjectType } from 'graphql';
 import { graphqlBodySchema } from './schema';
+
+const schemaQuery = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    memberType: queryMemberType,
+    memberTypes: queryMemberTypes,
+    user: queryUser,
+    users: queryUsers,
+    profile: queryProfile,
+    profiles: queryProfiles,
+    post: queryPost,
+    posts: queryPosts
+  }
+});
+
+const schemaMutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    createUser: queryCreateUser,
+    updateUser: queryUpdateUser,
+    createProfile: queryCreateProfile,
+    updateProfile: queryUpdateProfile,
+    createPost: queryCreatePost,
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: schemaQuery,
+  mutation: schemaMutation
+});
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.post(
-    '/',
+  fastify.post('/',
     {
       schema: {
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      const { query, variables } = request.body;
+
+      return await graphql({
+        schema,
+        source: query!,
+        variableValues: variables,
+        contextValue: fastify
+      });
+    }
   );
 };
 
