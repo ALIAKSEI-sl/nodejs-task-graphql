2b6472a6d1ba20c4180d0a775c6101e0f0b48828
Aliaksei <sladkiy.aleksey@gmail.com>
2023-01-30 19:05:16 +0100

    feat: add graphqlQuerySchema and graphqlMutationSchema in schema

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,3 +1,14 @@
+import { GraphQLObjectType } from 'graphql';
+import { memberTypeConfig, memberTypesConfig } from './member-types/memberTypesConfig';
+import { userConfig, usersConfig } from './users/usersConfig';
+import { profileConfig, profilesConfig } from './profiles/profilesConfig';
+import { postConfig, postsConfig } from './posts/postsConfig';
+import { createUserConfig } from './users/createUserConfig';
+import { updateUserConfig } from './users/updateUserConfig';
+import { createProfileConfig } from './profiles/createProfileConfig';
+import { updateProfileConfig } from './profiles/updateProfileConfig';
+import { createPostConfig } from './posts/createPostConfig';
+
 export const graphqlBodySchema = {
   type: 'object',
   properties: {
@@ -32,3 +43,28 @@ export const graphqlBodySchema = {
     },
   ],
 } as const;
+
+export const graphqlQuerySchema = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    memberType: memberTypeConfig,
+    memberTypes: memberTypesConfig,
+    user: userConfig,
+    users: usersConfig,
+    profile: profileConfig,
+    profiles: profilesConfig,
+    post: postConfig,
+    posts: postsConfig
+  }
+});
+
+export const graphqlMutationSchema = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    createUser: createUserConfig,
+    updateUser: updateUserConfig,
+    createProfile: createProfileConfig,
+    updateProfile: updateProfileConfig,
+    createPost: createPostConfig,
+  }
+});
